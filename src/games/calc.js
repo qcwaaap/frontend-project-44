7359b2417be0e522011592361c99e49c42ff85bc
Masha <masikemokid@gmail.com>
2025-02-05 11:07:09 +0300

    9 tasks done

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,3 +1,4 @@
+import { question } from 'readline-sync';
 import { getRandomNum } from '../utils.js';
 const getCount = (randomNum, operator, randomNum2) => {
     switch (operator) {
@@ -17,7 +18,7 @@ const getCount = (randomNum, operator, randomNum2) => {
             const randomNum = getRandomNum();
             const randomNum2 = getRandomNum();
             const randomOperator = point[randomPoint]
-            const que = randomNum + randomOperator + randomNum2;
+            const question = randomNum + randomOperator + randomNum2;
             const answ = String(getCount(randomNum, randomOperator, randomNum2))
-            return [que, answ];
+            return [question, answ];
         };
