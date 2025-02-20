0aec6b4bb7c9f3adcb6242618673adee31dc068d
Masha <masikemokid@gmail.com>
2025-02-20 15:11:12 +0300

    9 tasks done

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,13 +5,14 @@ export const rule = 'What number is missing in the progression?';
 export const getQuestionAndAnswer = () => {
   const start = getRandomNum();
   const step = getRandomNum();
-  const que = [start];
-  for (let i = 1; i <= 9; i += 1) {
-    que[i] = que[i - 1] + step;
+  const progression = [start];
+  const progressionLength = 9;
+  for (let i = 1; i <= progressionLength; i += 1) {
+    progression[i] = progression[i - 1] + step;
   }
   const index = getRandomNum(10);
-  const answer = String(que[index]);
-  const p = [...que];
-  p[index] = '..';
-  return [p.join(' '), answer];
+  const answer = String(progression[index]);
+  const que = [...progression];
+  que[index] = '..';
+  return [que.join(' '), answer];
 };
